3e8469d6f3183733df6cbd23f00a37169cb8b05b
SangLv <lsang2884@gmail.com>
2025-02-28 00:08:51 +0700

    add page dich vu

--- wp-content/themes/hoangkim/footer.php
+++ wp-content/themes/hoangkim/footer.php
@@ -49,98 +49,91 @@
                                     </div>
                                 </div>
                             </li>
+                            <li>
+                                <div class="social-wrap">
+                                    <a rel="nofollow" href="todo" class="social">
+                                        <i class="fa-classic fa-brands fa-facebook" aria-hidden="true"></i>
+                                    </a>
+                                    <a rel="nofollow" href="todo" class="social">
+                                        <i class="fa-classic fa-brands fa-twitter" aria-hidden="true"></i>
+                                    </a>
+                                    <a rel="nofollow" href="todo" class="social">
+                                        <i class="fa-classic fa-brands fa-youtube" aria-hidden="true"></i>
+                                    </a>
+                                    <a rel="nofollow" href="todo" class="social">
+                                        <i class="fa-classic fa-brands fa-instagram" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                            </li>
                         </ul>
                     </div>
                 </div>
-                <!-- <div class="col-12 col-md-6 col-lg-6">
+                <div class="col-12 col-md-6 col-lg-6">
                     <div class="row">
                         <div class=" col-12  col-md-6 col-lg-6">
                             <div class="title-footer">Dịch vụ</div>
                             <div class="menu-dich-vu-container">
                                 <ul id="menu-footer-2" class="menu-class">
                                     <li>
-                                        <a href="todo">Order Taobao</a>
+                                        <a href="<?php echo site_url() ?>/dich-vu/dat-hang-taobao/">Order Taobao</a>
                                     </li>
                                     <li>
-                                        <a href="todo">Order Pinduoduo</a>
+                                        <a href="<?php echo site_url() ?>/dat-hang-1688/">Đặt hàng 1688</a>
                                     </li>
                                     <li>
-                                        <a href="todo">Đặt hàng 1688</a>
+                                        <a href="<?php echo site_url() ?>/van-chuyen-hang-trung-quoc-ve-viet-nam/">Vận chuyển hàng Trung Quốc</a>
                                     </li>
                                     <li>
-                                        <a href="todo">Đặt hàng Alibaba</a>
-                                    </li>
-                                    <li>
-                                        <a href="todo">Vận chuyển hàng Trung Quốc</a>
-                                    </li>
-                                    <li>
-                                        <a href="todo">Đặt hàng Quảng Châu</a>
-                                    </li>
-                                    <li>
-                                        <a href="todo" aria-current="page">Đặt hàng Trung Quốc</a>
+                                        <a href="<?php echo site_url() ?>/dat-hang-quang-chau/">Đặt hàng Quảng Châu</a>
                                     </li>
                                 </ul>
                             </div>
-                        </div> -->
-                <div class=" col-12  col-md-6 col-lg-6">
-                    <div class="title-footer">Chính sách</div>
-                    <div class="menu-chinh-sach-container">
-                        <ul id="menu-footer-3" class="menu-class">
-                            <li>
-                                <a rel="nofollow" href="<?php echo site_url() ?>/chinh-sach-bao-mat-thong-tin/">Chính sách bảo mật thông tin</a>
-                            </li>
-                            <li>
-                                <a rel="nofollow" href="<?php echo site_url() ?>/dieu-khoan-su-dung-website/">Điều khoản sử dụng website</a>
-                            </li>
-                            <li>
-                                <a rel="nofollow" href="<?php echo site_url() ?>/chinh-sach-cookie/">Chính sách cookie</a>
-                            </li>
-                            <li>
-                                <a rel="nofollow" href="<?php echo site_url() ?>/chinh-sach-doi-tra-hang/">Chính sách đổi trả hàng</a>
-                            </li>
-                            <li>
-                                <a rel="nofollow" href="<?php echo site_url() ?>/chinh-sach-giai-quyet-khieu-nai-hang-ki-gui/">Chính sách giải quyết khiếu nại với hàng ký gửi</a>
-                            </li>
-                            <li>
-                                <a rel="nofollow" href="<?php echo site_url() ?>/chinh-sach-khieu-nai/">Chính sách khiếu nại</a>
-                            </li>
-                            <li>
-                                <a rel="nofollow" href="<?php echo site_url() ?>/chinh-sach-mua-hang/">Chính sách mua hàng</a>
-                            </li>
-                            <li>
-                                <a rel="nofollow" href="<?php echo site_url() ?>/chinh-sach-van-chuyen/">Chính sách vận chuyển</a>
-                            </li>
-                            <li>
-                                <a rel="nofollow" href="<?php echo site_url() ?>/danh-muc-hang-cam-nhap-khau/">Danh mục hàng cấm nhập khẩu</a>
-                            </li>
-                        </ul>
+                        </div>
+                        <div class=" col-12  col-md-6 col-lg-6">
+                            <div class="title-footer">Chính sách</div>
+                            <div class="menu-chinh-sach-container">
+                                <ul id="menu-footer-3" class="menu-class">
+                                    <li>
+                                        <a rel="nofollow" href="<?php echo site_url() ?>/chinh-sach-bao-mat-thong-tin/">Chính sách bảo mật thông tin</a>
+                                    </li>
+                                    <li>
+                                        <a rel="nofollow" href="<?php echo site_url() ?>/dieu-khoan-su-dung-website/">Điều khoản sử dụng website</a>
+                                    </li>
+                                    <li>
+                                        <a rel="nofollow" href="<?php echo site_url() ?>/chinh-sach-cookie/">Chính sách cookie</a>
+                                    </li>
+                                    <li>
+                                        <a rel="nofollow" href="<?php echo site_url() ?>/chinh-sach-doi-tra-hang/">Chính sách đổi trả hàng</a>
+                                    </li>
+                                    <li>
+                                        <a rel="nofollow" href="<?php echo site_url() ?>/chinh-sach-giai-quyet-khieu-nai-hang-ki-gui/">Chính sách giải quyết khiếu nại với hàng ký gửi</a>
+                                    </li>
+                                    <li>
+                                        <a rel="nofollow" href="<?php echo site_url() ?>/chinh-sach-khieu-nai/">Chính sách khiếu nại</a>
+                                    </li>
+                                    <li>
+                                        <a rel="nofollow" href="<?php echo site_url() ?>/chinh-sach-mua-hang/">Chính sách mua hàng</a>
+                                    </li>
+                                    <li>
+                                        <a rel="nofollow" href="<?php echo site_url() ?>/chinh-sach-van-chuyen/">Chính sách vận chuyển</a>
+                                    </li>
+                                    <li>
+                                        <a rel="nofollow" href="<?php echo site_url() ?>/danh-muc-hang-cam-nhap-khau/">Danh mục hàng cấm nhập khẩu</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="social-wrap">
-                <a rel="nofollow" href="todo" class="social">
-                    <i class="fa-classic fa-brands fa-facebook" aria-hidden="true"></i>
-                </a>
-                <a rel="nofollow" href="todo" class="social">
-                    <i class="fa-classic fa-brands fa-twitter" aria-hidden="true"></i>
-                </a>
-                <a rel="nofollow" href="todo" class="social">
-                    <i class="fa-classic fa-brands fa-youtube" aria-hidden="true"></i>
-                </a>
-                <a rel="nofollow" href="todo" class="social">
-                    <i class="fa-classic fa-brands fa-instagram" aria-hidden="true"></i>
-                </a>
+        </div>
+        <div class="end-footer">
+            <div class="container">
+                <div class="row wrapper">
+                    <div class="copyright">© Copyright 2025 Văn phòng xuất nhập khẩu Hoàng Kim Logistics</div>
+                </div>
             </div>
         </div>
-    </div>
-    </div>
-    <div class="end-footer">
-        <div class="container">
-            <div class="row wrapper">
-                <div class="copyright">© Copyright 2025 Văn phòng xuất nhập khẩu Hoàng Kim Logistics</div>
-            </div>
-        </div>
-    </div>
     </div>
 </footer>
 <?php wp_footer(); ?>
